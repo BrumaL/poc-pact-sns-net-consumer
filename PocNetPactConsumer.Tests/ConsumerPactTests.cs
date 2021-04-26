8a39b0c5fe14afe7b056821d39186b011b7bc8cb
Martin Eklund <martin.eklund@xlent.se>
2021-04-26 16:46:33 +0200

    fix contract

--- PocNetPactConsumer.Tests/ConsumerPactTests.cs
+++ PocNetPactConsumer.Tests/ConsumerPactTests.cs
@@ -26,7 +26,7 @@ namespace PocNetPactConsumer.Tests
                     Some.Object.Named("MessageAttributes").With(
                         Some.Object.Named("ID").With(
                             Some.Element.Named("DataType").Like("number"),
-                            Some.Element.Named("StringValue").Like(234)
+                            Some.Element.Named("StringValue").Like("3")
                             ),
                         Some.Object.Named("Name").With(
                             Some.Element.Named("DataType").Like("string"),
@@ -62,39 +62,5 @@ namespace PocNetPactConsumer.Tests
                 .BuildAsync();
            stubRepo.Received(1).SaveProduct(productCreated);
         }
-
-        // PactNet message-pact feature branch function
-        //[Fact]
-        //public void Handle_WhenProductIsCreated_SavesProduct()
-        //{
-        //    var stubRepo = Substitute.For<ProductRepository>();
-        //    var consumer = new ProductService(stubRepo);
-        //    var product = new Product { Id = 1, Name = "Polestar 1" };
-
-        //    var providerStates = new[]
-        //    {
-        //        new ProviderState
-        //        {
-        //            Name = "There is a product"
-        //        }
-        //     };
-
-        //    _messagePact.Given(providerStates)
-        //        .ExpectedToReceive("create product event")
-        //        .With(new Message
-        //        {
-        //            Contents = new
-        //            {
-        //                Message = "Some message",
-        //                MessageAttributes = new Dictionary<string, MessageAttribute>()
-        //                {   
-        //                    { "ID", new MessageAttribute { DataType = "number", StringValue = "3" }},
-        //                    { "Name", new MessageAttribute { DataType = "string", StringValue = "Polestar 3" }}
-        //                }
-        //            }
-        //        })
-        //        .VerifyConsumer<ProductCreated>(e => consumer.HandleSaveProductEvent(e));
-        //    stubRepo.Received(1).SaveProduct(product);
-        //}
     }
 }
